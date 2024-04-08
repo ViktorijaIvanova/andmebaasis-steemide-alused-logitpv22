4fdbd56b6541ca99ee37ee7b18474ad532abc69f
Viktorija Ivanova <vikomiller844@gmail.com>
2024-04-08 14:11:05 +0300

    insert into on valmis

--- tabeliloomine.sql
+++ tabeliloomine.sql
@@ -15,4 +15,9 @@ sisseastumis_kp date
 );
 -- primary key -primaarne võti mis annab unukaalssus
 --identity(1,1) -määrab igaühele oma numbri 
+--kuva kõik mis on tabelis
+SELECT * FROM opilane;
+--andmete lisamine 
+INSERT INTO opilane(eesnimi, perenimi, isikukood, aaddress, sisseastumis_kp)
+VALUES('Kari', 'Fudova', '60604120291', 'Tallinn', '2022-08-28');
 SELECT * FROM opilane;
