f2565e295b77a61bcae648aea80b68f2e2f5a6f2
Viktorija Ivanova <vikomiller844@gmail.com>
2024-04-08 14:48:33 +0300

    add table language

--- tabeliloomine.sql
+++ tabeliloomine.sql
@@ -21,3 +21,26 @@ SELECT * FROM opilane;
 INSERT INTO opilane(eesnimi, perenimi, isikukood, aaddress, sisseastumis_kp)
 VALUES('Kari', 'Fudova', '60604120291', 'Tallinn', '2022-08-28');
 SELECT * FROM opilane;
+---------------------------------------
+CREATE TABLE Language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage decimal(5,2)
+); 
+--tabeli andmete lisamine
+SELECT * FROM Language;
+INSERT INTO Language (ID, Code, Language, IsOfficial, Percentage)
+VALUES (100, 'EST', 'Eesti', 1, 80.5)
+-- TABELI STRUKTUURI MUUTMINE 
+ALTER TABLE Language ADD Capital varchar(20);
+--veergu kustutamine 
+ALTER TABLE Language DROP COLUMN Capital;
+ALTER TABLE Language ADD Capital varchar(20);
+INSERT INTO Language (ID, Code, Language, IsOfficial, Percentage, Capital)
+VALUES (101, 'FIN', 'Soome', 1, 90.5, 'Helsinki');
+--uuendamine 
+UPDATE Language SET Capital='Tallinn'
+WHERE ID=100;
