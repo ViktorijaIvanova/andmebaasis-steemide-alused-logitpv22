229957e88205eee6da24a0910c14fb60443f69e8
ViktorijaIvanova <120027756+ViktorijaIvanova@users.noreply.github.com>
2024-04-24 12:21:45 +0300

    Update proceduur.sql

--- proceduur.sql
+++ proceduur.sql
@@ -4,3 +4,16 @@ CREATE TABLE film (
   kestvus int,
   rezisorID varchar(50),
   v_aasta int;
+
+--uue protseduuri
+DELIMITER $$
+CREATE PROCEDURE `otsing1Taht`(IN `taht` CHAR(1) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER
+Begin 
+	SELECT * from film
+	WHERE filmNimetus LIKE  CONCAT(@taht,'%');
+End;
+
+
+
+--protseduuri käivitamine
+CALL otsing1Taht ('F');
