be6470ae17dfdf992b78c9b7adbcf45753432e1b
ViktorijaIvanova <120027756+ViktorijaIvanova@users.noreply.github.com>
2024-04-24 13:41:14 +0300

    Update proceduur.sql

--- proceduur.sql
+++ proceduur.sql
@@ -24,3 +24,16 @@ CREATE PROCEDURE `kustutaFilm`(IN `id` INT) NOT DETERMINISTIC CONTAINS SQL SQL S
 DELETE FROM film WHERE filmID=id;
 SELECT * FROM film; 
 END; 
+--проверяем через teosta
+
+
+--
+CREATE PROCEDURE `FilmdeArv`() NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT COUNT(*) AS 'filmide Arv' FROM film; END 
+
+Call 'FilmdeArv'();
+--
+CREATE PROCEDURE `lisaFilm` NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN insert into film (filmNimetus, kestvus, rezisoor, v_aasta) values (uusfilm, kestvus,rezisoor , aasta); SELECT * FROM film; END
+
+
+--
+CREATE PROCEDURE `uuendaRezisorFilmis`(IN `uusrezisoor` VARCHAR(50), IN `filmNimetus` VARCHAR(50)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN Select * from film WHERE filmNimetus='filmnimi'; UPDATE film SET rezisoor=uusrezisoor WHERE filmNimetus=filmNimetus; Select * from film where filmNimetus='filmNimi'; END; 
