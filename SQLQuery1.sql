448fd7035270a4ce9734d0a7de5a74ed8df611ea
Viktorija Ivanova <vikomiller844@gmail.com>
2024-05-08 12:37:57 +0300

    --add constraint __ check-kontrollib, kas kirjed töötavad uue piirangu lisamisel

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -89,7 +89,7 @@ update Person
 set Age = 149
 where Id = 8
 
---?
+--add constraint __ check-kontrollib, kas kirjed töötavad uue piirangu lisamisel
 alter table Person
 add constraint CK_Person_Age check (Age > 0 and Age < 150)
 
