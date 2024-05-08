5839795979bfb4e22ce95b1e377fe428bb777ad3
Viktorija Ivanova <vikomiller844@gmail.com>
2024-05-08 12:48:15 +0300

    näitab teatud vanusega inimesi

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -114,7 +114,7 @@ select * from Person where City = 'Gotham'
 select * from Person where City <> 'Gotham'
 select * from Person where City != 'Gotham'
 
--- näitab teatud vanusega inimesi
+--näitab teatud vanusega inimesi
 select *from Person where Age = 100 or 
 Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
