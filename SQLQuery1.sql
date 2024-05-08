82dfbf70f2a212176d431b0c2637cb215a45d757
Viktorija Ivanova <vikomiller844@gmail.com>
2024-05-08 12:32:55 +0300

    lisame võõrvõtme uuesti

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -66,7 +66,7 @@ drop constraint tblPerson_GenderId_FK
 --sisestame väärtuse tabelisse
 insert into Gender (Id, Gender)
 values (3, 'Unknown')
--- lisame võõrvõtme uuesti
+--lisame võõrvõtme uuesti
 alter table Person
 add constraint DF_Person_GenderId
 default 3 for GenderId
