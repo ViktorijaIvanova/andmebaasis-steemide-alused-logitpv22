5bbefe1cc3e057a3d5cd2df135871562efe63895
Viktorija Ivanova <vikomiller844@gmail.com>
2024-05-08 12:58:46 +0300

    võtab kolm esimest rida

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -139,7 +139,7 @@ select * from Person where Name like '[^WAS]%'
 select * from Person where (City = 'Gotham' or City = 'New York')
 and Age >= 40
 
----võtab kolm esimest rida
+--- võtab kolm esimest rida
 select top 3 * from Person
 
 --- kolm esimest, aga tabeli järjestus on Age ja siis Name
