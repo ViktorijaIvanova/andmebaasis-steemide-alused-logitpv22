cef9fffe9765078df930c253f1fc3c1e74c93289
Viktorija Ivanova <vikomiller844@gmail.com>
2024-05-08 13:00:25 +0300

    näitab esimesed 50% tabelis

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -146,7 +146,7 @@ select top 3 * from Person
 select * from Person
 select top 3 Age, Name from Person
 
---- näitab esimesed 50% tabelis
+---näitab esimesed 50% tabelis
 select top 50 percent * from Person
 --?
 select * from Person order by cast(Age as int)
